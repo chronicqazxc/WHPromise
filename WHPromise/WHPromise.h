85c08fa5da8333273edf56724bec3d7ff056a5b3
Wayne Hsiao <chronicqazxc@gmail.com>
2019-10-30 15:43:04 +0800

    support osx

--- WHPromise/WHPromise.h
+++ WHPromise/WHPromise.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double WHPromiseVersionNumber;
 FOUNDATION_EXPORT const unsigned char WHPromiseVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <WHPromise/PublicHeader.h>
-
-
